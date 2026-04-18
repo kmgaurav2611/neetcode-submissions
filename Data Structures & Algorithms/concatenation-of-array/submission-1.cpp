c6522a0f2dd49be9976be67a3b42badaabd8602e
Kumar Gaurav <kmgaurav2611@gmail.com>
2026-04-18 21:59:49 +0530

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> res(nums);
+
+        // for(int i = 0; i< nums.size(); i++) {
+        //     res.push_back(nums[i]);
+        // }
+        res.insert(res.end(), nums.begin(), nums.end());
+
+        return res;
+    }
+};
