4f2d6a704e449a6096b950a3412df360d0025f37
Kumar Gaurav <kmgaurav2611@gmail.com>
2026-08-06 21:59:27 +0530

    Add: n-th-tribonacci-number - submission-0

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int solve(int n , vector<int>& dp){
+        if(n ==0)
+        return 0;
+        if(n == 1)
+        return 1;
+        if( n==2)
+        return 1;
+
+        if(dp[n]!= -1)
+        return dp[n];
+
+        dp[n] = solve(n-1, dp)+ solve(n-2, dp) + solve(n-3, dp);
+        return dp[n];        
+    }
+    int tribonacci(int n) {
+        vector<int> dp(n+1, -1);
+
+        int result = solve(n, dp);
+        return result;
+    }
+};
